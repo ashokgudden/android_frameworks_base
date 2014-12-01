af78b26881f8fab4d5513599722c169a498bc757
Jon Evans <jon@craftyjon.com>
2014-11-30 20:55:42 -0500

    Port: Wake on plug (1/2)
    abc: set off by default, we have battery led
    use an user system setting instead of global
    
    Change-Id: I5e50a7fd351857d76c1c2fd13c6e737d8902a2f3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4015,6 +4015,14 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+       /**
+         * Whether to wake the display when plugging or unplugging the charger
+         *
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4189,6 +4197,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LESS_BORING_HEADS_UP);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
+            PRIVATE_SETTINGS.add(WAKE_WHEN_PLUGGED_OR_UNPLUGGED);
         }
 
         /**
