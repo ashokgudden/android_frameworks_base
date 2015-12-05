64bb5e42be81ff80b342594788bd27d1b319d831
eyosen <abittin@gmail.com>
2015-12-05 18:36:19 +0100

    Add Screen Record to the Power Menu [1/2]
    Change-Id: Ib1eff9f6ae5cc8b6927275604b4380a3e555d813

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4026,6 +4026,13 @@ public final class Settings {
        public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
 
         /**
+         * Whether to display the screenrecord option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SCREENRECORD = "powermenu_screenrecord";
+
+        /**
          *  Enable statusbar double tap gesture on to put device to sleep
          * @hide
          */
