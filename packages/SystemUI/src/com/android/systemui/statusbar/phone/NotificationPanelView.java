2dcbd870c79e8a5da817ad825ec98a90c44d8d74
Esteban Talavera <etalavera@google.com>
2017-07-14 16:14:07 +0100

    Enforce policy for camera gesture in keyguard
    If the admin has disabled the camera for secure
    keyguards, in addition to removing the bottom-right hand
    corner camera button do not allow the camera to be opened
    via the camera gesture either.
    
    Bug: 63334090
    Merged-In: I104688eaad719528376e2851f837d5956a6a1169
    Test: Manually tested launching the camera on secure and non-secure keyguard and non-keyguard, both via camera icon and gesture
    Change-Id: I104688eaad719528376e2851f837d5956a6a1169
    (cherry picked from commit 98ec92375d75630c10595bfcbae184ad7350e2d3)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2491,13 +2491,13 @@ public class NotificationPanelView extends PanelView implements
      *
      * @param keyguardIsShowing whether keyguard is being shown
      */
-    public boolean canCameraGestureBeLaunched(boolean keyguardIsShowing) {
+    public boolean canCameraGestureBeLaunched(boolean keyguardIsShowing, int source) {
         if (!mStatusBar.isCameraAllowedByAdmin()) {
             EventLog.writeEvent(0x534e4554, "63787722", -1, "");
             return false;
         }
 
-        ResolveInfo resolveInfo = mKeyguardBottomArea.resolveCameraIntent();
+        ResolveInfo resolveInfo = mKeyguardBottomArea.resolveCameraIntent(source);
         String packageToLaunch = (resolveInfo == null || resolveInfo.activityInfo == null)
                 ? null : resolveInfo.activityInfo.packageName;
         return packageToLaunch != null &&
