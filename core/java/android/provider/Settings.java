719ec24d3ebcfef289621d02ea42bb5d92b80278
maxwen <max.weninger@gmail.com>
2017-10-08 18:32:10 +0200

    [1/2] base: rework and cleanup network traffic
    Change-Id: I693e495b8b0f5d29ec38e47f7d51b71931a47942

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4030,6 +4030,11 @@ public final class Settings {
         // CUSTOM system settings
 
         /**
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_ENABLE = "network_traffic_enable";
+
+        /**
          * Network traffic indicator, goes from least to greatest significant bitwise
          * 0 = Display up-stream traffic if set
          * 1 = Display down-stream traffic if set
