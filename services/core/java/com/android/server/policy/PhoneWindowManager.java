0c8fe0be0d89521883990b321dae3c9dcbcef477
Mirko <fraz140@gmail.com>
2017-10-09 16:36:29 +0200

    Revert "Add Power+VolUp action to catch a partial area screenshot"
    This reverts commit 5d4c2f0d04bcc7cce8d3155a87984bab6adf2c01.
    
    Change-Id: I89125f6292cdfd64da418cf4ad03dd9158371b61

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1705,28 +1705,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mScreenshotChordEnabled
                 && mScreenshotChordVolumeDownKeyTriggered && mScreenshotChordPowerKeyTriggered
                 && !mA11yShortcutChordVolumeUpKeyTriggered) {
-            triggerScreenshotAction(true);
-        } else if (mScreenshotChordEnabled
-                && !mScreenshotChordVolumeDownKeyTriggered && mScreenshotChordPowerKeyTriggered
-                && mA11yShortcutChordVolumeUpKeyTriggered) {
-            triggerScreenshotAction(false);
-        }
-    }
-
-    private void triggerScreenshotAction(boolean isFullScreenshot) {
-        final long now = SystemClock.uptimeMillis();
-        if (now <= (isFullScreenshot ? mScreenshotChordVolumeDownKeyTime : mA11yShortcutChordVolumeUpKeyTime)
-                + SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS && now <= mScreenshotChordPowerKeyTime
-                        + SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS) {
-            if (isFullScreenshot) {
+            final long now = SystemClock.uptimeMillis();
+            if (now <= mScreenshotChordVolumeDownKeyTime + SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS
+                    && now <= mScreenshotChordPowerKeyTime
+                            + SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS) {
                 mScreenshotChordVolumeDownKeyConsumed = true;
-            } else {
-                mA11yShortcutChordVolumeUpKeyConsumed = true;
+                cancelPendingPowerKeyAction();
+                mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_FULLSCREEN);
+                mHandler.postDelayed(mScreenshotRunnable, getScreenshotChordLongPressDelay());
             }
-            cancelPendingPowerKeyAction();
-            mScreenshotRunnable.setScreenshotType(isFullScreenshot ? TAKE_SCREENSHOT_FULLSCREEN
-                    : TAKE_SCREENSHOT_SELECTED_REGION);
-            mHandler.postDelayed(mScreenshotRunnable, getScreenshotChordLongPressDelay());
         }
     }
 
@@ -3502,7 +3489,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // If we think we might have a volume down & power key chord on the way
         // but we're not sure, then tell the dispatcher to wait a little while and
         // try again later before dispatching.
-        /*if (mScreenshotChordEnabled && (flags & KeyEvent.FLAG_FALLBACK) == 0) {
+        if (mScreenshotChordEnabled && (flags & KeyEvent.FLAG_FALLBACK) == 0) {
             if (mScreenshotChordVolumeDownKeyTriggered && !mScreenshotChordPowerKeyTriggered) {
                 final long now = SystemClock.uptimeMillis();
                 final long timeoutTime = mScreenshotChordVolumeDownKeyTime
@@ -3518,14 +3505,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 return -1;
             }
-        }*/
+        }
 
         // If an accessibility shortcut might be partially complete, hold off dispatching until we
         // know if it is complete or not
-        if ((mAccessibilityShortcutController.isAccessibilityShortcutAvailable(false) || mScreenshotChordEnabled)
+        if (mAccessibilityShortcutController.isAccessibilityShortcutAvailable(false)
                 && (flags & KeyEvent.FLAG_FALLBACK) == 0) {
-            if ((mScreenshotChordVolumeDownKeyTriggered ^ mA11yShortcutChordVolumeUpKeyTriggered)
-                    && !mScreenshotChordPowerKeyTriggered) {
+            if (mScreenshotChordVolumeDownKeyTriggered ^ mA11yShortcutChordVolumeUpKeyTriggered) {
                 final long now = SystemClock.uptimeMillis();
                 final long timeoutTime = (mScreenshotChordVolumeDownKeyTriggered
                         ? mScreenshotChordVolumeDownKeyTime : mA11yShortcutChordVolumeUpKeyTime)
@@ -6222,7 +6208,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             mA11yShortcutChordVolumeUpKeyTime = event.getDownTime();
                             mA11yShortcutChordVolumeUpKeyConsumed = false;
                             cancelPendingPowerKeyAction();
-                            interceptScreenshotChord();
+                            cancelPendingScreenshotChordAction();
                             interceptAccessibilityShortcutChord();
                         }
                     } else {
