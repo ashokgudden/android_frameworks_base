11ae2d672d262fd45e880f6a11691e5f07464f60
maxwen <max.weninger@gmail.com>
2017-10-15 00:20:59 +0200

    [1/2] base: fixe changing doze settings
    thank you google for changing the DEVICE_IDLE_CONSTANTS from
    under the hood. So save our values in a different setting
    which is used before the system one if available
    
    Change-Id: I94dcd885b482299932c6ce6238852a1d290c76d8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10232,6 +10232,11 @@ public final class Settings {
         public static final String RINGTONE_FOCUS_MODE = "ringtone_focus_mode";
 
         /**
+         * @hide
+         */
+        public static final String DEVICE_IDLE_CONSTANTS_USER = "device_idle_constants_user";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
