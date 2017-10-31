6bcf52670eaf4a60e99055d1293292dab5660ad5
Ashok Soni <ashok.soni@gmail.com>
2017-10-31 15:06:36 +0530

    Revert "WakeBlock: Blocking the drain before it’s too late v2.0"
    This reverts commit e1be352723c40d79ae2277c3a4d637222aa31438.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1146,9 +1146,6 @@ public final class PowerManagerService extends SystemService
 
     private void acquireWakeLockInternal(IBinder lock, int flags, String tag, String packageName,
             WorkSource ws, String historyTag, int uid, int pid) {
-        // @ WAKEBLOCK
-        com.giovannibozzano.wakeblock.WakeBlockService.getInstance().bindService(mContext);
-        // # WAKEBLOCK
         synchronized (mLock) {
             if (DEBUG_SPEW) {
                 Slog.d(TAG, "acquireWakeLockInternal: lock=" + Objects.hashCode(lock)
@@ -1180,9 +1177,6 @@ public final class PowerManagerService extends SystemService
                     notifyWakeLockChangingLocked(wakeLock, flags, tag, packageName,
                             uid, pid, ws, historyTag);
                     wakeLock.updateProperties(flags, tag, packageName, ws, historyTag, uid, pid);
-                    // @ WAKEBLOCK
-                    com.giovannibozzano.wakeblock.WakeBlockService.getInstance().wakeLockUpdateProperties(lock, wakeLock.mTag, tag);
-                    // # WAKEBLOCK
                 }
                 notifyAcquire = false;
             } else {
@@ -1261,9 +1255,6 @@ public final class PowerManagerService extends SystemService
             }
 
             WakeLock wakeLock = mWakeLocks.get(index);
-            // @ WAKEBLOCK
-            com.giovannibozzano.wakeblock.WakeBlockService.getInstance().wakeLockRelease(lock, wakeLock.mTag);
-            // # WAKEBLOCK
             if (DEBUG_SPEW) {
                 Slog.d(TAG, "releaseWakeLockInternal: lock=" + Objects.hashCode(lock)
                         + " [" + wakeLock.mTag + "], flags=0x" + Integer.toHexString(flags));
@@ -1290,9 +1281,6 @@ public final class PowerManagerService extends SystemService
                 return;
             }
 
-            // @ WAKEBLOCK
-            com.giovannibozzano.wakeblock.WakeBlockService.getInstance().wakeLockHandleDeath(wakeLock.mLock, wakeLock.mTag);
-            // # WAKEBLOCK
             removeWakeLockLocked(wakeLock, index);
         }
     }
