e36b6a17122de402eac1f45f53703851a17fa4a6
ezio84 <brabus84@gmail.com>
2017-12-04 16:43:29 +0100

    DUI: don't initialize pulse and media arrows for some apps [1/2]
    pretty useless in those cases
    
    Change-Id: I6bcc59ab6f8408d67d9858b7f55f2a2a8d113e98

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -638,6 +638,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     protected PorterDuffXfermode mSrcOverXferMode =
             new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER);
 
+    private String[] mNavMediaArrowsExcludeList;
     private MediaSessionManager mMediaSessionManager;
     private MediaController mMediaController;
     private String mMediaNotificationKey;
@@ -707,6 +708,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                     getMediaControllerPlaybackState(mMediaController)
                     || PlaybackState.STATE_BUFFERING ==
                     getMediaControllerPlaybackState(mMediaController)) {
+                final String currentPkg = mMediaController.getPackageName().toLowerCase();
+                for (String packageName : mNavMediaArrowsExcludeList) {
+                    if (currentPkg.contains(packageName)) {
+                        return;
+                    }
+                }
                 mNavigationBar.setMediaPlaying(true);
             } else {
                 mNavigationBar.setMediaPlaying(false);
@@ -1098,6 +1105,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         };
         Dependency.get(ConfigurationController.class).addCallback(mConfigurationListener);
+
+        final String list = mContext.getResources().getString(R.string.navMediaArrowsExcludeList);
+        mNavMediaArrowsExcludeList = list.split(",");
     }
 
     protected void createIconController() {
